895e36e32bb51949c661500e99641dfa3b0ec067
Allegra <Allegra.Weston@ligentia.global>
2025-08-23 19:48:46 +0100

    Fixed base uri doh!

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/cv-editor/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/CVEditor/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',

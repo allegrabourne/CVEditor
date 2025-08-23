c1877ffc60f42ec2451bf63797a29c6ab433079b
Allegra <Allegra.Weston@ligentia.global>
2025-08-23 20:55:40 +0100

    Updated CV

--- src/components/CVEditor.jsx
+++ src/components/CVEditor.jsx
@@ -7,7 +7,7 @@ const CVEditor = () => {
       name: "Allegra Bourne",
       phone: "(+44) 7854 943 193",
       address: "14 Lewis Road, Northallerton, North Yorkshire, UK. DL6 3RX",
-      email: "allegrabourne@gmail.com",
+      email: "alleweston@gmail.com",
       website: "allegrabourne.github.io/"
     },
     profile: "Software Engineer with 15+ years of experience delivering event driven, AI powered and scalable solutions for local, national and multinational businesses in both the private and public sectors across diverse industries, including finance, supply chain and transportation systems.\n\nAdept at translating business requirements into technical solutions and working with modern development frameworks, cloud services, and enterprise messaging solutions with an emphasis on test-driven development and design patterns.\n\nRecognised for mentoring, collaboration, and building systems that enhance operational performance.",
@@ -37,6 +37,121 @@ const CVEditor = () => {
           "Azure development utilising blob containers.",
           "Emphasis on TDD and design patterns to ensure quality of work."
         ]
+      },
+      {
+        title: "Integrations Developer",
+        company: "XCM, York, England, United Kingdom",
+        dates: "Apr 2020 - Apr 2021",
+        description: "XCM are marketing specialists working with some of the biggest retailers in the UK to leverage competitive advantage via the use of specialist data tools, analysis and CRM services.",
+        responsibilities: [
+          "Client integration utilising .Net Core and Kafka to ensure real time distributed streaming of big data from Google PubSub.",
+          "Docker used for Kafka containerisation.",
+          "TSQL / KSQL development.",
+          "Google Cloud / PubSub integration.",
+          "Apache AVRO for binary serialisation of messages for faster processing.",
+          "Data migration and integration.",
+          "Single Customer View.",
+          "Development of the XCM Horizon data warehouse.",
+          "Web Services development using .Net Core hosted in Kestrel."
+        ]
+      },
+      {
+        title: "Software Engineer",
+        company: "Cubic Corporation, Stockton-On-Tees, United Kingdom",
+        dates: "Jan 2019 - Apr 2020",
+        description: "Cubic Transportation Systems is one arm of Cubic Corporation that produces public transport solutions, traffic management platforms and fare reading and payment systems.",
+        responsibilities: [
+          "Development of ICMP - the traffic management platform utilised by New South Wales, Australia. The platform formed part of TfNSW's $123 million intelligent congestion management program which enabled better traffic management through real-time data and predictive technology.",
+          "Frontend development utilising Angular / Typescript / SignalR.",
+          "Enterprise messaging conducted utilising NServiceBus / RabbitMQ.",
+          "Micro service development utilising AZURE Service Fabric / Topshelf / WebAPI services.",
+          "Continuous integration via Jenkins with SonarCube code smells and Octopus.",
+          "Development undertaken accordingly with Agile/SCRUM methodologies."
+        ]
+      },
+      {
+        title: "Solution Developer",
+        company: "Vianet Group plc, Stockton-On-Tees, United Kingdom",
+        dates: "Jun 2017 - Dec 2018",
+        description: "Vianet are UK leader in providing draught beer quality and waste management services.",
+        responsibilities: [
+          "Worked on the IDraught IOT platform on the consumption of data that would empower customer's strategic decisions on metrics and business intelligence, these insights were then used to measure metrics such as wastage, temperature, in identifying missing drinks and improving drink quality.",
+          "Frontend development on the new 'Vianet IOT Portal' utilising AngularJS 2 / Typescript.",
+          "WebAPI services via the use of .Net Core and hosted using Kestrel.",
+          "AZURE web app services and message queueing.",
+          "Big data processing via HBASE.",
+          "JAVA / Apache SPARK / Hadoop number crunching business logic utilising IntelliJ GUI and taking on a TDD approach.",
+          "Introduced an emphasis on TDD and introduced concepts attaining to Agile/SCRUM and mentoring junior members of the team."
+        ]
+      },
+      {
+        title: "Lead Software Developer",
+        company: "ZyroFisher, Darlington, United Kingdom",
+        dates: "Apr 2017 - Jun 2017",
+        description: "ZyroFisher are the leading distributor of Parts, Accessories and Clothing to the UK and Irish cycling markets.",
+        responsibilities: [
+          "Integrated new Altura Cycling customer facing commercial website with their backend ordering system and payment provider (included implementing Sage/SagePay API) utilising RabbitMQ, RESTful API services and HMAC authentication enabling customers to make online purchases for the first time.",
+          "Front-end development utilising NodeJS.",
+          "Emphasis on quality test driven development utilising NCrunch and MOQ."
+        ]
+      },
+      {
+        title: "Software Developer",
+        company: "Lowell Group, Leeds, United Kingdom",
+        dates: "Apr 2012 - Apr 2017",
+        description: "Lowell are one of Europe's largest credit management companies, operating in the UK and the DACH and Nordic regions.",
+        responsibilities: [
+          "RESTful Web API services self hosted utilising Topshelf.",
+          "MVC5/Rest/CMS Umbraco - development of the new Lowell website, designed to give customers control over their accounts and manage payments securely through Verifone, similar to leading banking websites. In its first year, the platform saw over 150,000 customer registrations.",
+          "Automated deployment via Octopus deploy and continuous builds using TeamCity.",
+          "Test driven .Net development. NCrunch, MOQ.",
+          "Service Orientated Architecture - development of person matching and address cleansing services utilising WCF and RabbitMQ.",
+          "T-SQL utilising CLR functions.",
+          "ORM implemented using NHibernate.",
+          "Agile software development utilising SCRUM.",
+          "Development of LIMA - Lowell's main tracing application.",
+          "Provided third line support as required."
+        ]
+      },
+      {
+        title: "Collections Development Analyst",
+        company: "Lowell, Leeds, United Kingdom",
+        dates: "Feb 2011 - Apr 2012",
+        description: "",
+        responsibilities: [
+          "Core business process mapping and improvement.",
+          "Business process automation utilising SSIS, T-SQL and C#/VB.net in order to create dynamic DTSX packages.",
+          "Data analysis utilising Crystal Reports and SSAS.",
+          "Systems support, project and change management.",
+          "Automated de-duping processes for enabling single view of customers with multiple accounts saving Lowell £40k (quoted by system supplier to scope out possible automation).",
+          "Mentored colleagues in the use of source control, TFS, .NET and SSIS."
+        ]
+      },
+      {
+        title: "Integrated Systems Officer",
+        company: "North Yorkshire County Council, Northallerton, United Kingdom",
+        dates: "Feb 2009 - Feb 2011",
+        description: "",
+        responsibilities: [
+          "Project managed the deployment of CACI's 'Children's Centre Management System to the 40 Children's Centres throughout North Yorkshire.",
+          "Project managed the deployment of North Yorkshire's new integrated online Admissions system.",
+          "Aided in the deployment and integration of IMPULSE; the integrated system for children in North Yorkshire.",
+          "Instigated a drive in data quality, this included the creation of a number of XML cleansing macros utilising VBA and xpath to cleanse data received from Schools.",
+          "Aided with the implementation and accreditation of the prescribed national child reference system - ContactPoint and took the lead during Instance Accreditation for North Yorkshire and partners.",
+          "Provided augmented services when required."
+        ]
+      },
+      {
+        title: "Systems Administrative Assistant (Internship)",
+        company: "State Street, Luxembourg",
+        dates: "Sep 2006 - Sep 2007",
+        description: "State Street Corporation is a financial services company that provides investment management services for institutional investors.",
+        responsibilities: [
+          "Data warehousing / mining and core applications development for fund compliance.",
+          "Java, Oracle and PL/SQL development.",
+          "VBA (Contingency macros).",
+          "Worked on the development of a data warehousing solution which pooled various assets, funds and investment information from different locations throughout the world. This information would then be used for investment analysis, fund accounting and various other investment strategies."
+        ]
       }
     ],
     personalProjects: [
@@ -54,11 +169,11 @@ const CVEditor = () => {
     certificates: [
       {
         title: "IBM Data Science Professional Certificate",
-        description: "A comprehensive, ACE-recommended (12 college credits) and FIBAA-accredited (6 ECTS credits) program covering key data science skills, including Python, regression analysis, statistical modeling, and machine learning."
+        description: ""
       },
       {
         title: "IBM AI Developer Professional Certificate",
-        description: "A comprehensive program focused on the essential skills for developing AI applications, including machine learning, deep learning, and natural language processing."
+        description: ""
       }
     ],
     education: {

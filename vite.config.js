72cb7f1c1db2ed23807089e2fb3e7966675989c5
Allegra <Allegra.Weston@ligentia.global>
2025-08-23 19:46:15 +0100

    Fixed vitejs issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cv-editor/',
+  base: process.env.NODE_ENV === 'production' ? '/cv-editor/' : '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 3000,
   },
 })
